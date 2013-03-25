60d88f85fccdd4d0540e20ee28893caa71df6f22
Wesley Turner <wes.turner@kitware.com>
2013-03-25 16:18:02 -0400

    Initial testing commit to establish the framework
    This is a copy of the CTest Tutorial. We will use this as a template to
    support our agile processes as we build out the tools and applications.

---
+++ Testing/CTestConfig.cmake
@@ -0,0 +1,9 @@
+set(CTEST_PROJECT_NAME "SemanticHealthcare")
+set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=SemanticHealthcare")
+set(CTEST_DROP_SITE_CDASH TRUE)
+set(CTEST_CDASH_VERSION "1.6")
+set(CTEST_CDASH_QUERY_VERSION TRUE)
+
